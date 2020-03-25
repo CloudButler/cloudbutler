307ce5b06da99c48bdf9ab30bd2724ceb03aa26b
Charles Christensen <30779702+charlesrc19@users.noreply.github.com>
2020-03-24 19:11:04 -0600

    Update quick-start.sh

--- quick-start.sh
+++ quick-start.sh
@@ -13,9 +13,9 @@ docker-compose up -d
 sleep 30
 
 # Configure helper bundles.
-echo "COG_ADMIN_PASSWORD: '${COG_ADMIN_PASSWORD}'" >> bundles/helper/layers/base.yml
-docker exec -it cloudbutler_cog_1 "cogctl bundle install bundles/helper/config.yaml -e -r default"
+echo "COG_ADMIN_PASSWORD: '${COG_ADMIN_PASSWORD}'" >> bundles/helper/layers/base.yaml
+docker exec -it cloudbutler_cog_1 cogctl bundle install bundles/helper/config.yaml -e -r default
 sleep 10
-docker exec -it cloudbutler_cog_1 "cogctl bundle config create helper bundles/helper/layers/base.yaml --layer=base"
+docker exec -it cloudbutler_cog_1 cogctl bundle config create helper bundles/helper/layers/base.yaml --layer=base
 sleep 10
 
